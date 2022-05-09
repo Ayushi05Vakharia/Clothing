501ea9fb4eddae81b85acffff15f6532fdd9f89a
Ayushi05Vakharia <ayushi.vakharia@gmail.com>
2022-05-09 18:45:38 +0530

    Remove unwanted code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import DisplayCollections from "./pages/DisplayCollections";
-// import Hats from "./pages/Hats/Hats";
-// import Jackets from "./pages/Jackets";
-// import Sneakers from "./pages/Sneakers";
-// import Womens from "./pages/Womens";
-// import Mens from "./pages/Mens";
+
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 
@@ -16,11 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<DisplayCollections />} />
-        {/* <Route path='/hats' element={<Hats/>}/>
-      <Route path='/jackets' element={<Jackets/>}/>
-      <Route path='/sneakers' element={<Sneakers/>}/>
-      <Route path='/womens' element={<Womens/>}/>
-      <Route path='/mens' element={<Mens/>}/> */}
       </Routes>
     </BrowserRouter>
   );
