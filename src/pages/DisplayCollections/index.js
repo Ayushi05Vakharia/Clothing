6a32a29616df72482540b89eb61f7c8452bd00b1
Ayushi05Vakharia <vakhariaayushi@gmail.com>
2022-05-09 18:48:08 +0530

    Remove unwanted code

--- src/pages/DisplayCollections/index.js
+++ src/pages/DisplayCollections/index.js
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import { shop_Data } from "../../Constants";
 import CollectionItems from "../CollectionItems";
-// import Jackets from '../Jackets'
-// import Sneakers from '../Sneakers'
-// import Womens from '../Womens'
-// import Mens from '../Mens'
-// import { Link } from 'react-router-dom'
 import "../../css/collection-items.sytles.scss";
 
 const DsiplayCollections = (props) => {
